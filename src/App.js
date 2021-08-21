0183539706f0d8bd99fc26754c9db1b613c4feaf
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-21 18:12:16 -0400

    idx on Scroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './styles.css'
 import Card1 from './Card1'
 
 function App() {
+  const [idx, setIdx] = useState(1); 
+  
+  const iterate = () => {
+    setIdx(old => old + 1)
+  }
+
   return (
-    <div className="scene">
-      <Card1 />
+    <div className="scene" onScroll={iterate}>
+      <Card1 num = {idx}/>
     </div>); 
 }
 
