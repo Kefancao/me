2f4f7282cce4c8b3b00dcf75d6a23a294a93f140
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-21 18:12:03 -0400

    Flip animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './styles.css'
+import Card1 from './Card1'
 
 function App() {
-  return (<></>); 
+  return (
+    <div className="scene">
+      <Card1 />
+    </div>); 
 }
 
 export default App;
--- src/Card1.js
+++ src/Card1.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import CV from './cv.png'
+import GitHubIcon from './github.png'
+import LinkedInIcon from './linkedin.png'
+import SendLogo from './send.png'
 
 export default function Card1() {
+
+	const card = useRef(); 
+	const flip = () => {
+		console.log(card.current.classList.toggle('is-flipped')); 
+	}
 	return (
-		<div>
-			
+		<div ref={card} className="card" onClick={flip}>
+			<div className="face front">
+				<h3 className="name">Kefan Cao</h3>
+				<h3 className="career">Software Developer</h3>
+				<div className="external-links">
+					<a href="https://github.com/Kefancao" target="_blank" rel="noreferrer"><img src={GitHubIcon} alt="GitHub"></img></a>
+					<a href="https://www.linkedin.com/in/kefan-cao-2801bb165/" target="_blank" rel="noreferrer"><img src={LinkedInIcon} alt="LinkedIn"></img></a>
+					<a href="google.ca" target="_blank" rel="noreferrer"><img src={CV} alt="Resume"></img></a>
+				</div>
+				<div id="connect">
+					<div id="input-box">
+						<input type="text" placeholder="Leave your email to connect!"></input>
+						<a href="google.ca"><img src={SendLogo} alt="Send"></img></a>
+					</div>
+				</div>
+				<div className="next">
+					<h1>&gt;</h1>
+				</div>
+			</div>
+			<div className="face back">Hi</div>
 		</div>
 	)
 }
