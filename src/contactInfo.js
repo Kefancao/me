8219472b83d1c03b3032f816004003a6100a036b
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-22 12:46:37 -0400

    Added a message textarea comp

--- src/contactInfo.js
+++ src/contactInfo.js
@@ -1,26 +1,38 @@
 import React, {useState} from 'react'
 import SendLogo from './send.png'
+import './contact-style.css'
 
 export default function Contact() {
 	const [clicked, setClicked] = useState(false); 
 	const [name, setName] = useState(''); 
 	const [email, setEmail] = useState('')
+	const [message, setMessage] = useState('')
 
-	const handleName = (e)=> setName(e.current.value); 
-	const handleEmail = (e)=> setEmail(e.current.value); 
+	const handleName = (e)=> setName(e.target.value); 
+	const handleEmail = (e)=> setEmail(e.target.value); 
 
 	return (
 		<div className="contact">
 			{!clicked ? <img onClick = {()=> {setClicked(old => !old)}} src={SendLogo} id = "send-icon" alt="Send"></img> : 
 				<div className="contact-box">
-					<p>Thank you for visiting this website. If you'd like to get in touch, please fill in the form below!</p>
-					<form id="contact-form">
-						<label>Name: </label>
-						<input type="text" value={name} placeholder="Enter your name" onChange={e => handleName(e)}></input>
-						<label>Email: </label>
-						<input type="text" value={email} placeholder="Enter your Email" onChange={e => handleEmail(e)}></input>
+					<div id='exit-contact' onClick = {()=> {setClicked(old => !old)}}><h1>x</h1></div>
+					<div id="contact-wrap">
+						<p>Thank you for visiting this website. If you'd like to get in touch, please fill in the form below!</p>
+						<div id="name-form">
+							<label for="name">Name: </label>
+							<input type="text" id="name" value={name} placeholder="Enter your name" onChange={e => handleName(e)}></input>
+						</div>
+						<div id="email-form">
+							<label for="email">Email: </label>
+							<input type="text" id="email" value={email} placeholder="Enter your Email" onChange={e => handleEmail(e)}></input>
+						</div>
+
+						<div id="message-area">
+							<label for="message">Message: </label>
+							<textarea onChange={e => handleName(e)} value={message} placeholder="Enter a message"></textarea>
+						</div>
 						<button>Submit</button>
-					</form>
+					</div>
 				</div>
 			}
 		</div>
