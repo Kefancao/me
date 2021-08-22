5b0563ed1686d5ca988614ef366791a52bf65c11
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-22 19:20:55 -0400

    Graphics and more style

--- src/Card1.js
+++ src/Card1.js
@@ -2,6 +2,7 @@ import React, {useRef} from 'react'
 import CV from './cv.png'
 import GitHubIcon from './github.png'
 import LinkedInIcon from './linkedin.png'
+import WaterlooIcon from './waterloo.svg'
 
 export default function Card1(props) {
 
@@ -18,7 +19,7 @@ export default function Card1(props) {
 		<div className="scene">
 			<div style = {{opacity : props.opac}} ref={card} className="card" onClick={flip} onScroll={e => scroll(e)}>
 				<div className="face front">
-					<h3 className="name">First Last</h3>
+					<h3 className="name">Kefan Cao</h3>
 					<h3 className="career">Software Developer</h3>
 					<div className="external-links">
 						<a href="https://github.com/Kefancao" target="_blank" rel="noreferrer"><img src={GitHubIcon} alt="GitHub"></img></a>
@@ -29,7 +30,18 @@ export default function Card1(props) {
 						<h1>&gt;</h1>
 					</div> 
 				</div>
-				<div className="face back">Hi</div>
+				<div className="face back">
+					<div className="content-container">
+						<h3>Welcome!!!</h3>
+						<p>
+							My name is Kefan, a software developer and Computer Science student at the University of Waterloo.
+						</p><br></br>
+						<p>
+							This website is a show case of what I've been working on. If anything here interests you, I'm always down to talk about it. You can reach 
+							me by clicking the contact button on the bottom left corner to send a message!
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
