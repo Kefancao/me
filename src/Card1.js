fcbb528e3bb24f3059964e7864a09c0f1f5b0fae
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-22 11:55:24 -0400

    Got rid of contact form in card

--- src/Card1.js
+++ src/Card1.js
@@ -2,7 +2,6 @@ import React, {useRef} from 'react'
 import CV from './cv.png'
 import GitHubIcon from './github.png'
 import LinkedInIcon from './linkedin.png'
-import SendLogo from './send.png'
 
 export default function Card1(props) {
 
@@ -25,20 +24,13 @@ export default function Card1(props) {
 						<a href="https://github.com/Kefancao" target="_blank" rel="noreferrer"><img src={GitHubIcon} alt="GitHub"></img></a>
 						<a href="https://www.linkedin.com/in/kefan-cao-2801bb165/" target="_blank" rel="noreferrer"><img src={LinkedInIcon} alt="LinkedIn"></img></a>
 						<a href="google.ca" target="_blank" rel="noreferrer"><img src={CV} alt="Resume"></img></a>
-					</div>
-					<div id="connect">
-						<div id="input-box">
-							<input type="text" placeholder="Leave your email to connect!" onClick={flip}></input>
-							<a href="google.ca"><img src={SendLogo} alt="Send"></img></a>
-						</div>
-					</div>
+					</div> 
 					<div className="next">
 						<h1>&gt;</h1>
 					</div> 
 				</div>
 				<div className="face back">Hi</div>
 			</div>
-			<div className="ref">HI</div>
 		</div>
 	)
 }
