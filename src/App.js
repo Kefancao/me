55c61e062ff4433c1d6683e74ff00f3ed80ed3ac
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-22 22:03:33 -0400

    Altered the opacity setting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Card1 from "./Card1";
 import Contact from "./contactInfo";
+import Cube1 from "./Cube1";
 
 
 function App() {
@@ -20,11 +21,11 @@ function App() {
   return (
     <>
     <div className="outer-container">
-      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.75)} />
-      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.75)} />
-      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.75)} />
-      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.75)} />
-      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.75)} />
+      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.70)} />
+      <Cube1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.70)} />
+      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.70)} />
+      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.70)} />
+      <Card1 opac={(Math.cos((window.pageYOffset* 2 * Math.PI)/ window.innerHeight) + 0.70)} />
     </div>
     <Contact /> 
     </>
