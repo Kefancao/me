a189934443588b13105c186e7bfaf564f1b38eb2
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-24 15:44:57 -0400

    Added description to each cube

--- src/Cube1.js
+++ src/Cube1.js
@@ -3,6 +3,73 @@ import './cube.css'
 import LeftNav from './leftArrow.svg'
 import RightNav from './rightArrow.svg'
 
+function Description(props){
+
+	switch(props.cur % 6){
+		case 1: 
+		return (
+			<div className="description">
+				<h1>Quick Chat</h1>
+				<p>
+					Quick Chat is a real-time chat application made using react and firebase. 
+					The app allows the user to enter a room key to join a room. 
+					The user can choose to join anonymously or sign in via their Google Account.
+				</p>
+			</div>
+		); 
+		case 3: 
+		return (
+			<div className="description">
+				<h1>kefancao.com</h1>
+				<p>
+					The website that you're looking at is kefancao.com :) It's created utilizing 
+					3D transform techniques of css in combination with React. The 3D cube transformations 
+					applies concepts from linear algebra and trignometry. 
+				</p>
+			</div>
+		); 
+		case 2: 
+		return(
+			<div className="description">
+				<h1>TypeWriterX</h1>
+				<p>
+					TypeWriterX is a web app that allows users to test their typing speed. Their typing speed are
+					displayed in WPM (words per minute). It is created using React and Firebase (to store user history).
+				</p>
+			</div>
+		); 
+		case 5:
+			return (<div className="description">
+				<h1>River Deliver</h1>
+				<p>River Delivery is a grocery/errands company and winner of a $3,000 Entrepeneurship fund from a pool of over 70 applicants. 
+					It is developed using React, Firebase, and Email.js (allows email notifications when the user places an order). It 
+					attracted over 100 users/customers throughout the summer and netted over $4,000 in revenue. 
+				</p>
+			</div>); 
+		case 0: 
+		return (<div className="description">
+				<h1>Compiler</h1>
+				<p>
+					Created in response to a final project. It provides support for basic arithmetic operations, loops, as well as recursive function evaluations.
+					It is created in C using the continuation and trampoline design patterns to maximize efficiency. 
+				</p>
+			</div>); 
+		case 4: 
+			return(<div className="description">
+				<h1>Biquadris</h1>
+				<p>
+					The biquadris game is an alternate version of traditional tetris with the main difference being that players enable a special action
+					to be enacted on the other player if the current player clears two rows. The game is written in C++ using concepts of object oriented programming
+					and design patterns including the factory pattern, the observer pattern, and the controller pattern. The game supports both a text-only mode or
+					a textGUI mode to be specified at run time. 
+				</p>
+			</div>); 
+		default: 
+			return <></>; 
+	}
+
+}
+
 export default function Cube1(props) {
 	const cube = useRef(); 
 	const [cur, setCur] = useState(301); 
@@ -23,21 +90,14 @@ export default function Cube1(props) {
 				<div className="cube-container">
 					<div ref={cube} className="cube">
 						<div className="cube__face cube__face--front">Quick Chat</div>
-						<div className="cube__face cube__face--back">back</div>
-						<div className="cube__face cube__face--right">right</div>
-						<div className="cube__face cube__face--left">left</div>
-						<div className="cube__face cube__face--top">top</div>
-						<div className="cube__face cube__face--bottom">bottom</div>
+						<div className="cube__face cube__face--back">kefancao.com</div>
+						<div className="cube__face cube__face--right">TypeWriterX</div>
+						<div className="cube__face cube__face--left">River Deliver</div>
+						<div className="cube__face cube__face--top">Compiler</div>
+						<div className="cube__face cube__face--bottom">Biquadris</div>
 					</div>
 				</div>
-				<div className="description">
-					<h1>Quick Chat</h1>
-					<p>
-						Quick Chat is a real-time chat application made using react and firebase. 
-						The app allows the user to enter a room key to join a room. 
-						The user can choose to join anonymously or sign in via their Google Account.
-					</p>
-				</div>
+				{<Description cur={cur}/>} 
 			</div>
 		</div>
 	)
